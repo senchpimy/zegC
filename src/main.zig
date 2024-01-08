74de371ed74829dcb16cd2f4a9e2c043aee371a2
senchpimy <senchpimy@gmail.com>
2024-01-08 11:04:24 -0600

    declaring variables

--- src/main.zig
+++ src/main.zig
@@ -5,9 +5,10 @@ const io = std.io;
 const mem = std.mem;
 const os = std.os;
 
-var types = [_][]const u8{ "int", "char", "long", "bool", "void" };
-const Type = enum { int, char };
+const primitive_types = [_][]const u8{ "int", "char", "long", "bool", "void", "double" };
+const Type = enum { int, char, long, bool, void, double };
 const Prompt = enum { start, none, cont };
+const ParsingError = error{Type};
 
 const Variable = struct { type: Type, value: u8 };
 const stdout = std.io.getStdOut().writer();
@@ -66,9 +67,12 @@ pub fn main() !void {
         } else if (std.ascii.isASCII(char)) {
             if (char == '\r') {
                 if (buf[index - 1] == ';') {
-                    _ = try parse_str(&buf);
+                    parse_str(&buf) catch |err| switch (err) {
+                        else => {},
+                    };
                     promt_v = .start;
                     @memset(&buf, 0);
+                    index = 0;
                     try stdout.print("\n", .{});
                     continue;
                 }
@@ -103,24 +107,56 @@ fn get_string(index: u32, char: u8) void {
     buf[index] = char;
 }
 
-fn parse_str(str: []u8) !i8 {
-    var splits = std.mem.split(u8, str, " ");
+fn parse_str(str: []u8) !void {
+    _ = str;
+    var splits = std.mem.split(u8, buf[0..], " ");
     var len: usize = 0;
     while (splits.next()) |_| {
         len += 1;
     }
     splits.reset();
-    return 0;
-    //var first = splits.next().?;
-    //var v_type: Type = if (cmp_type(first, 1)) {
-    //    .int;
-    //} else if (false) {
-    //    .char;
-    //};
-    //var v: Variable = Variable{ .type = v_type, .value = undefined };
-    //return v;
+    if (len == 2) { //variable creation but no assignation
+        var first = splits.next().?;
+        var v_type: Type = undefined;
+        var type_index: i16 = 0;
+        for (primitive_types) |typ| {
+            if (std.mem.eql(u8, first, typ)) {
+                break;
+            }
+            type_index += 1;
+        } else {
+            type_index = -1;
+        }
+        switch (type_index) {
+            0 => {
+                v_type = .int;
+            },
+            1 => {
+                v_type = .char;
+            },
+            2 => {
+                v_type = .long;
+            },
+            3 => {
+                v_type = .bool;
+            },
+            4 => {
+                v_type = .void;
+            },
+            5 => {
+                v_type = .double;
+            },
+            else => {
+                std.debug.print("\n '{s}' is not a type", .{first});
+                return ParsingError.Type;
+            },
+        }
+        var v: Variable = Variable{ .type = v_type, .value = undefined };
+        try variables.put(splits.next().?, v);
+        std.debug.print("{any}", .{v});
+    }
 }
 
 fn cmp_type(str: []const u8, index: usize) bool {
-    return std.mem.eql(u8, str, types[index]);
+    return std.mem.eql(u8, str, primitive_types[index]);
 }
